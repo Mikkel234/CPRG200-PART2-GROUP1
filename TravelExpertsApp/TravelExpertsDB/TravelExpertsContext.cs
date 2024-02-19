e854527451999a19f97a75e0ea95bbc8aed14879
Mikkel <Mikkel.Giesbrecht@edu.sait.ca>
2024-02-19 12:11:41 -0700

    TEST

--- TravelExpertsApp/TravelExpertsDB/TravelExpertsContext.cs
+++ TravelExpertsApp/TravelExpertsDB/TravelExpertsContext.cs
@@ -56,7 +56,7 @@ public partial class TravelExpertsContext : DbContext
     public virtual DbSet<TripType> TripTypes { get; set; }
 
     //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-    //    => optionsBuilder.UseSqlServer("Data Source=localhost\\sqlexpress;Initial Catalog=TravelExperts;Integrated Security=True; TrustServerCertificate=true");
+    //    => optionsBuilder.UseSqlServer("Data Source=localhost\\sqlexpress;Initial Catalog=TravelExperts;Integrated Security=True; TrustServerCertificate=true");   
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
